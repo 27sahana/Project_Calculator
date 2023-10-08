9b0f8beb925c11b101a9f5c7015d79dd48315031
Sahana P <sahana.p@publicissapient.com>
2023-10-08 21:50:03 +0530

    added few lines

--- src/main/java/com/example/ProjectCal/CalculatorController.java
+++ src/main/java/com/example/ProjectCal/CalculatorController.java
@@ -5,64 +5,64 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
- 
-
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
 
- 
-
+    // Constructor to inject CalculatorService dependency
     @Autowired
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
     }
 
- 
-
+    // Endpoint for calculating square root
     @GetMapping("/square-root")
     public ResponseEntity<Double> squareRoot(@RequestParam double number) {
         try {
+            // Call the calculatorService to calculate the square root
             double result = calculatorService.calculateSquareRoot(number);
+            // Return a successful response with the result
             return ResponseEntity.ok(result);
         } catch (IllegalArgumentException e) {
-            // Handle invalid input (e.g., negative number)
+            // Handle invalid input (e.g., negative number) with a bad request response
             return ResponseEntity.badRequest().body(null);
         } catch (Exception e) {
-            // Handle other unexpected exceptions
+            // Handle other unexpected exceptions with an internal server error response
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 
- 
-
+    // Endpoint for calculating factorial
     @GetMapping("/factorial")
     public ResponseEntity<Long> factorial(@RequestParam int number) {
         try {
+            // Call the calculatorService to calculate the factorial
             long result = calculatorService.calculateFactorial(number);
+            // Return a successful response with the result
             return ResponseEntity.ok(result);
         } catch (IllegalArgumentException e) {
-            // Handle invalid input (e.g., negative number)
+            // Handle invalid input (e.g., negative number) with a bad request response
             return ResponseEntity.badRequest().body(null);
         } catch (Exception e) {
-            // Handle other unexpected exceptions
+            // Handle other unexpected exceptions with an internal server error response
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 
- 
-
+    // Endpoint for calculating modulus
     @GetMapping("/modulus")
     public ResponseEntity<Integer> modulus(@RequestParam int dividend, @RequestParam int divisor) {
         try {
+            // Call the calculatorService to calculate the modulus
             int result = calculatorService.calculateModulus(dividend, divisor);
+            // Return a successful response with the result
             return ResponseEntity.ok(result);
         } catch (ArithmeticException e) {
-            // Handle division by zero
+            // Handle division by zero with a bad request response
             return ResponseEntity.badRequest().body(null);
         } catch (Exception e) {
-            // Handle other unexpected exceptions
+            // Handle other unexpected exceptions with an internal server error response
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
--- src/main/java/com/example/ProjectCal/CalculatorServiceImpl.java
+++ src/main/java/com/example/ProjectCal/CalculatorServiceImpl.java
@@ -4,31 +4,35 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
-    @Override
-    public double calculateSquareRoot(double number) {
-        return Math.sqrt(number);
-    }
+	// Calculates the square root of a given number.
+	@Override
+	public double calculateSquareRoot(double number) {
+		return Math.sqrt(number);
+	}
 
-    @Override
-    public long calculateFactorial(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
-        }
-        if (number == 0 || number == 1) {
-            return 1;
-        }
-        long result = 1;
-        for (int i = 2; i <= number; i++) {
-            result *= i;
-        }
-        return result;
-    }
+	// Calculates the factorial of a non-negative integer.
+	@Override
+	public long calculateFactorial(int number) {
+		if (number < 0) {
+			throw new IllegalArgumentException("Factorial is not defined for negative numbers.");
+		}
+		if (number == 0 || number == 1) {
+			return 1;
+		}
+		long result = 1;
+		// Calculate the factorial using a loop.
+		for (int i = 2; i <= number; i++) {
+			result *= i;
+		}
+		return result;
+	}
 
-    @Override
-    public int calculateModulus(int dividend, int divisor) {
-        if (divisor == 0) {
-            throw new IllegalArgumentException("Division by zero is not allowed.");
-        }
-        return dividend % divisor;
-    }
+	// Calculates the modulus of two integers, avoiding division by zero.
+	@Override
+	public int calculateModulus(int dividend, int divisor) {
+		if (divisor == 0) {
+			throw new IllegalArgumentException("Division by zero is not allowed.");
+		}
+		return dividend % divisor;
+	}
 }
